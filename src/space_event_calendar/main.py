631960a2c2d6bc5e903ddaa6ef3798d03a925e07
Viepertech <59316357+Viepertech@users.noreply.github.com>
2025-07-01 21:17:31 -0400

    Create main.py

---
+++ src/space_event_calendar/main.py
@@ -0,0 +1,8 @@
+def get_upcoming_events():
+   #TODO: change static data to polled data
+    return [
+        {"name": "Perseid Meteor Shower Peak", "date": "2025-08-12"},
+        {"name": "Partial Lunar Eclipse", "date": "2025-09-17"},
+        {"name": "Supermoon", "date": "2025-10-08"},
+        {"name": "Venus–Jupiter Conjunction", "date": "2025-11-02"},
+    ]
