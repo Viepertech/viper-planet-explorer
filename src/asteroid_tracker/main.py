1459c5a69fbd7953a53c89bf50a7df8b11bdc4c6
Viepertech <59316357+Viepertech@users.noreply.github.com>
2025-10-22 22:26:38 -0400

    Update imports to use relative paths

--- src/asteroid_tracker/main.py
+++ src/asteroid_tracker/main.py
@@ -1,8 +1,8 @@
 # main.py
-from datetime import date, timedelta
-from api_client import get_neo_feed_data
-from data_processing import process_neo_data_for_plot
-from visualization import create_3d_asteroid_plot, save_spinning_html
+from .datetime import date, timedelta
+from .api_client import get_neo_feed_data
+from .data_processing import process_neo_data_for_plot
+from .visualization import create_3d_asteroid_plot, save_spinning_html
 
 def run_asteroid_tracker():
     today = date.today()
